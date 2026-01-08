63452e124053519ff44a8d0fd9772573db876b1f
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2026-01-08 15:12:10 -0300

    Add tests for login and user role update routes.

---
+++ test/users/update-user-role.test.ts
@@ -0,0 +1,54 @@
+import { describe, expect } from 'vitest'
+import type { ActiveUserSelect } from '../../src/database/schema/users.ts'
+import type { UserRole } from '../../src/schemas/role.ts'
+import { server } from '../../src/server.ts'
+import { test } from '../fixtures.ts'
+
+describe('PATCH /users/update-role/:id', () => {
+  test.scoped({ userRole: 'manager' })
+
+  test('should change user role', async ({ user, token }) => {
+    const newRole: UserRole = 'regular'
+
+    const roleUpdateResponse = await server.inject({
+      method: 'PATCH',
+      url: `/users/update-role/${user.id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      payload: {
+        role: newRole,
+      },
+    })
+
+    expect(roleUpdateResponse.statusCode).toBe(204)
+
+    const getUserResponse = await server.inject({
+      method: 'GET',
+      url: '/users',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    const fetchedUser = getUserResponse.json().users[0] as ActiveUserSelect
+
+    expect(fetchedUser.role).toBe(newRole)
+  })
+
+  test('should return 404 for non-existing user', async ({ token }) => {
+    const roleUpdateResponse = await server.inject({
+      method: 'PATCH',
+      url: '/users/update-role/00000000-0000-0000-0000-000000000000',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      payload: {
+        role: 'regular',
+      },
+    })
+
+    expect(roleUpdateResponse.statusCode).toBe(404)
+    expect(roleUpdateResponse.json()).toEqual({ message: 'User not found' })
+  })
+})
